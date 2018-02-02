ba0c390118ebe4d47a7e5b79f1c9ca13b12e2884
Gian Mina <gsmina@ubiquitous-tech.com>
2018-02-03 02:05:57 +0800

    Installed GuzzleHtml and integrated Google reCAPTCHA to user registration.

--- resources/lang/en/validation.php
+++ resources/lang/en/validation.php
@@ -87,6 +87,7 @@ return [
     'unique'               => 'The :attribute has already been taken.',
     'uploaded'             => 'The :attribute failed to upload.',
     'url'                  => 'The :attribute format is invalid.',
+    'recaptcha'            => 'Please ensure that you are a human!',
 
     /*
     |--------------------------------------------------------------------------
