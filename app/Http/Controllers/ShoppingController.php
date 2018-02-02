4742a158f6addec718bfc9888ff83743af790724
Gian Mina <gsmina@ubiquitous-tech.com>
2018-02-03 04:42:23 +0800

    Added models.

---
+++ app/Http/Controllers/ShoppingController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Product;
+
+class ShoppingController extends Controller
+{
+    public function addToCart(Product $item)
+    {
+
+    }
+}
