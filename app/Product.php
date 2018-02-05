f6db60be681d99ac29f4c5c7cc98053b12e7d6a2
Gian Mina <gsmina@ubiquitous-tech.com>
2018-02-05 15:07:11 +0800

    Implemented the Buyable interface in Product.

--- app/Product.php
+++ app/Product.php
@@ -3,8 +3,19 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Buyable;
 
-class Product extends Model
+class Product extends Model implements Buyable
 {
-    //
+    public function getBuyableIdentifier($options = null) {
+        return id;
+    }
+
+    public function getBuyableDescription($options = null) {
+        return name;
+    }
+
+    public function getBuyablePrice($options = null) {
+        return price;
+    }
 }
